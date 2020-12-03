b33e36aa5a5fe753360e9d90ee92f80b26e4b57e
Hunter Nemitz <hunter.nemitz@gmail.com>
2020-12-03 18:29:29 -0500

    Edits

--- day02/day02.py
+++ day02/day02.py
@@ -4,15 +4,16 @@ from aocd import get_data
 data = [d for d in get_data(day=2).splitlines()]
 
 def isValid(input: str):
-    min, max, char, pwd = re.split('-|:\\s|\\s', input)
+    min, max, char, pwd = re.split(r': |-|\s', input)
     min, max = int(min), int(max)
 
-    return [min <= pwd.count(char) <= max, (pwd[min-1] == char ^ pwd[max-1] == char)]
+    return [min <= pwd.count(char) <= max, (pwd[min-1] == char) ^ (pwd[max-1] == char)]
 
 
 c1, c2 = 0, 0
 for d in data:
-    res = isValid(d)
-    c1 += res[0]
-    c2 += res[1]
-print(c1, c2)
+    count,pos = isValid(d)
+    c1 += count
+    c2 += pos
+print(f"The number of valid passwords by count is {c1}.") 
+print(f"The number of valid passwords by position is {c2}.")
