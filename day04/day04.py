99d62daed22c2ebc90524aaf3faa4c57a882f8de
Hunter Nemitz <hunter.nemitz@gmail.com>
2020-12-04 17:52:50 -0500

    Further modifications

--- day04/day04.py
+++ day04/day04.py
@@ -6,24 +6,18 @@ presence = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 isValid = False
 part1, part2 = 0, 0
 
-def byr(value): 
-    return 1920 <= int(value) <= 2002
-def iyr(value): 
-    return 2010 <= int(value) <= 2020
-def eyr(value): 
-    return 2020 <= int(value) <= 2030
+def byr(value): return 1920 <= int(value) <= 2002
+def iyr(value): return 2010 <= int(value) <= 2020
+def eyr(value): return 2020 <= int(value) <= 2030
+def hcl(value): return re.search(r"#[0-9a-f]{6}\Z", value)
+def ecl(value): return value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+def pid(value): return re.search(r"\A\d{9}\Z", value)
 def hgt(value):
     if not re.search(r"\A\d{2,3}\D{2}\Z", value):
         return False
     num,mes = value[:-2], value[-2:]
     min, max = (150, 193) if mes == "cm" else (59, 76)
     return min <= int(num) <= max
-def hcl(value): 
-    return re.search(r"\A[#]\w{6}\Z", value)
-def ecl(value):
-    return value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
-def pid(value): 
-    return re.search(r"\A\d{9}\Z", value)
 
 processor = {"byr": byr, "iyr": iyr, "eyr": eyr, "hgt": hgt, "hcl": hcl, "ecl": ecl, "pid": pid}
 
@@ -38,5 +32,5 @@ for line in data:
             if not isValid:
                 break
         if isValid: part2 += 1
-print(f"The number of passports with all required fields is {part1}.")
-print(f"The number of passports with all required fields and valid values is {part2}.") 
+print(f"Passports w/ all fields: {part1}")
+print(f"Passports w/ all fields and valid values: {part2}") 
