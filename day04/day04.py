4093180e40ce8b4ef42aad498e967af6bc53d4eb
Hunter Nemitz <hunter.nemitz@gmail.com>
2020-12-04 17:14:57 -0500

    Finished Day 4 and Cleaned Up

--- day04/day04.py
+++ day04/day04.py
@@ -2,9 +2,9 @@ from aocd import get_data
 import copy
 import re
 data = get_data(day=4).split("\n\n")
-data = [data[x].split() for x in range(len(data))]
-presence = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"] 
-valid = 0
+presence = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+isValid = False
+part1, part2 = 0, 0
 
 def byr(value): 
     return 1920 <= int(value) <= 2002
@@ -16,8 +16,7 @@ def hgt(value):
     if not re.search(r"\A\d{2,3}\D{2}\Z", value):
         return False
     num,mes = value[:-2], value[-2:]
-    min = 150 if mes == "cm" else 59
-    max = 193 if mes == "cm" else 76
+    min, max = (150, 193) if mes == "cm" else (59, 76)
     return min <= int(num) <= max
 def hcl(value): 
     return re.search(r"\A[#]\w{6}\Z", value)
@@ -25,35 +24,19 @@ def ecl(value):
     return value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
 def pid(value): 
     return re.search(r"\A\d{9}\Z", value)
-def cid(value):
-    return True
 
-def processor(field) -> bool:
-    op, val = field.split(":")
-    print(op, val)
-    switcher = {
-        "byr": byr,
-        "iyr": iyr,
-        "eyr": eyr,
-        "hgt": hgt,
-        "hcl": hcl,
-        "ecl": ecl,
-        "pid": pid
-    }
-    if op == "cid": return True
-    func = switcher.get(op)
-    if func: 
-        return func(val)
-    else: 
-        return False
+processor = {"byr": byr, "iyr": iyr, "eyr": eyr, "hgt": hgt, "hcl": hcl, "ecl": ecl, "pid": pid}
 
-for i in range(10):
-    check = []
-    for j in range(len(data[i])):
-        if not processor(data[i][j]):
-            break
-        if data[i][j][:3] != "cid": 
-            check.append(data[i][j][:3])
-    if check.sort() == presence.sort():
-        valid += 1
-print(valid) 
+for line in data:
+    line = {x.split(":")[0]: x.split(":")[1] for x in line.split()}
+    f = [i for i in line.keys() if i in presence]
+    if len(f) == len(presence):
+        part1 += 1
+        for field in presence:
+            func = processor.get(field)
+            isValid = func(line[field])
+            if not isValid:
+                break
+        if isValid: part2 += 1
+print(f"The number of passports with all required fields is {part1}.")
+print(f"The number of passports with all required fields and valid values is {part2}.") 
