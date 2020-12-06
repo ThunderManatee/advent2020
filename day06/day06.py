0c62e8cbe3278830eebfd7d6197788c9dbd02f48
Hunter Nemitz <hunter.nemitz@gmail.com>
2020-12-06 15:05:24 -0500

    Added ascii_lowercase

--- day06/day06.py
+++ day06/day06.py
@@ -1,7 +1,8 @@
 from aocd import get_data
+from string import ascii_lowercase
 data = get_data(day=6).split("\n\n")
 p1, p2 = 0, 0
-alpha = "abcdefghijklmnopqrstuvwxyz"
+alpha = ascii_lowercase
 for line in data:
     ansList = list(line.replace("\n", ""))
     p1 += len(set(ansList))
@@ -9,5 +10,5 @@ for line in data:
     for letter in alpha:
         if ansList.count(letter) == groupSize:
             p2 += 1
-print(p1)
-print(p2)
+print(f"Any member answered: {p1}")
+print(f"All members answered: {p2}")
