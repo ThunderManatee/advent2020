4055ae3c363d4a997e9190b8a7cb5fd50afe6753
Hunter Nemitz <hunter.nemitz@gmail.com>
2020-12-09 13:42:31 -0500

    Day 9 Complete

--- day09/day09.py
+++ day09/day09.py
@@ -1,8 +1,11 @@
 from aocd import get_data
 data = get_data(day=9).splitlines()
 data = [int(x) for x in data]
+#data = [35, 20, 15, 25, 47, 40, 62, 55, 65, 
+#95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576]
+data1 = data.copy()
 
-def sumcheck(pre, num):
+def sumCheck(pre, num):
     a = 0
     pre.sort()
     while len(pre) > 0:
@@ -15,12 +18,27 @@ def sumcheck(pre, num):
             return True
     return False 
 
-sumset = [data.pop(0) for _ in range(25)]
-while len(data) > 0:
-    checknum = data[0]
-    if sumcheck(sumset.copy(), checknum) == True:
+checkNum = 0
+sumset = [data1.pop(0) for _ in range(25)]
+while len(data1) > 0:
+    checkNum = data1.pop(0)
+    if sumCheck(sumset.copy(), checkNum) == True:
         sumset.pop(0)
-        sumset.append(data.pop(0))
+        sumset.append(checkNum)
     else:
-        print(f"Invalid number: {checknum}")
+        print(f"Invalid number: {checkNum}")
+        data.pop(data.index(checkNum))
         break
+ind = 0
+dFlag = False
+while dFlag == False:
+    valArr = [data.pop(0)]
+    for i in data:
+        valArr.append(i)
+        if sum(valArr) > checkNum:
+            break
+        if sum(valArr) == checkNum:
+            valArr.sort()
+            print(f"Encryption Weakness: {valArr[0] + valArr[-1]}")
+            dFlag = True
+            break
