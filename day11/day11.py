a5ca373b29567e9d536b25d08f010034dceb53c1
Hunter Nemitz <hunter.nemitz@gmail.com>
2020-12-11 19:53:29 -0500

    Day 11 Complete

---
+++ day11/day11.py
@@ -0,0 +1,53 @@
+from copy import deepcopy
+from aocd import get_data
+data = get_data(day=11).splitlines()
+data = [[char for char in line] for line in data]
+lenC, lenR = len(data)-1, len(data[0])-1
+
+def get_sight_map(boat, adj):
+    boatDict = {}
+    for indR, row in enumerate(boat):
+        for indS in range(len(row)):
+            if boat[indR][indS] != ".":
+                boatDict[(indR, indS)] = []
+                searchMap = [(-1, -1), (-1, 0), (-1, 1),
+                            (0,-1), (0, 1),
+                            (1, -1), (1, 0), (1, 1)]
+                searchMap = [(x,y) for x,y in searchMap if 0 <= indR+x <= lenC and 0 <= indS+y <= lenR]
+                for i,j in searchMap:
+                    x, y = indR+i, indS+j
+                    while 0 <= x <= lenC and 0 <= y <= lenR:
+                        if data[x][y] not in ("#", "L"):
+                            if adj == True:
+                                break
+                            x, y = x+i, y+j
+                        else:
+                            boatDict[(indR, indS)].append((x,y))
+                            break
+    return boatDict
+
+def seat_checker(map, data , oCheck):
+    oldChart = deepcopy(data)
+    newChart = deepcopy(data)
+    while True:
+        occCount = 0
+        oldChart = deepcopy(newChart)
+        for (x, y), checks in map.items():
+            seat = oldChart[x][y]
+            dirtyDegens = 0
+            for i,j in checks:
+                if oldChart[i][j] == "#":
+                    dirtyDegens += 1
+            if seat == "L" and dirtyDegens == 0:
+                newChart[x][y] = "#"
+                occCount += 1
+            elif seat == "#" and dirtyDegens >= oCheck:
+                newChart[x][y] = "L"
+            elif seat == "#" and dirtyDegens < oCheck:
+                occCount += 1
+        if newChart == oldChart:
+            break
+    return occCount
+
+print(f"Predicted Occupied Seats: {seat_checker(get_sight_map(data, True), data, 4)}")
+print(f"Actual Occupied Seats: {seat_checker(get_sight_map(data, False), data, 5)}")
